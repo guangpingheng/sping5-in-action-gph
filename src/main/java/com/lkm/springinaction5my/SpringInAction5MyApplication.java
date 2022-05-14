2dfd8c57d7c34a1331279b714afe75b1532a6ca4
李昆明 <xtulkm@163.com>
2022-05-14 17:40:25 +0800

    init all

---
+++ src/main/java/com/lkm/springinaction5my/SpringInAction5MyApplication.java
@@ -0,0 +1,13 @@
+package com.lkm.springinaction5my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInAction5MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInAction5MyApplication.class, args);
+    }
+
+}
